33df8280697890d72a744af4ba6e0dbf1c91fd6f
Regina Galambos <gina.galambos@gmail.com>
2019-12-29 20:34:43 +0100

    create action to list all entries from API Add date formatting to display in table

--- bptracker/frontend/src/actions/entries.js
+++ bptracker/frontend/src/actions/entries.js
@@ -1,1 +1,17 @@
 // All HTTP requests here
+import axios from "axios";
+
+import { GET_ENTRIES } from './types';
+
+// GET ENTRIES
+export const getEntries = () => dispatch => {
+    axios
+        .get('/api/entries/')
+        .then(res => {
+            dispatch({
+                type: GET_ENTRIES,
+                payload: res.data
+            });
+        })
+        .catch(err => console.log(err));
+}
--- bptracker/frontend/src/components/entries/EntriesList.js
+++ bptracker/frontend/src/components/entries/EntriesList.js
@@ -1,13 +1,69 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { connect } from "react-redux";
+import PropTypes from 'prop-types';
+
+import { getEntries } from "../../actions/entries";
 
 export class EntriesList extends Component {
+    static propTypes = {
+        entries: PropTypes.array.isRequired
+    };
+
+    componentDidMount() {
+        this.props.getEntries();
+    }
+
+    // https://stackoverflow.com/questions/58634980/format-created-at-date-in-reactjs
+    // https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/DateTimeFormat
+    
+    dateTimeFormatter = new Intl.DateTimeFormat('de-DE', {
+        timeZone: 'UTC',
+        year: 'numeric',
+        month: 'numeric',
+        day: '2-digit',
+        hour: 'numeric',
+        minute: 'numeric'
+    });
+
     render() {
         return (
-            <div>
-                <h1>Entries List</h1>
-            </div>
+            <Fragment>
+                <h2>Entries</h2>
+                <div className="table-container">
+                    <table className="table is-striped is-hoverable">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Systolic</th>
+                                <th>Diastolic</th>
+                                <th>Pulse</th>
+                                <th>Created at</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { this.props.entries.map(entry => (
+                                <tr key={entry.id}>
+                                    <td>{entry.id}</td>
+                                    <td>{entry.systolic}</td>
+                                    <td>{entry.diastolic}</td>
+                                    <td>{entry.pulse}</td>
+                                    <td>{this.dateTimeFormatter.format(new Date(entry.created_at))}</td>
+                                    <td>
+                                        <button className="button is-danger is-small">Delete</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </Fragment>
         )
     }
 }
 
-export default EntriesList
+const mapStateToProps = state => ({
+    entries: state.entries.entries
+});
+
+export default connect(mapStateToProps, { getEntries })(EntriesList);
