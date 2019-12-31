ec13c62d891837c404e424cbd26f9fc5da6bed2a
Regina Galambos <gina.galambos@gmail.com>
2019-12-31 01:04:40 +0100

    create 'accounts' app to handle user auth add Register serializers and api to Django User model implement token authentication with knox

--- bptracker/bptracker/settings.py
+++ bptracker/bptracker/settings.py
@@ -39,9 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'knox',
     'entries',
     'frontend',
+    'accounts',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication', )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
