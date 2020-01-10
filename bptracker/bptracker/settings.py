ea10ff0fb132ac5162eb8b5e2b334518f4d3346b
Regina Galambos <gina.galambos@gmail.com>
2020-01-10 03:54:20 +0100

    update static_root settings

--- bptracker/bptracker/settings.py
+++ bptracker/bptracker/settings.py
@@ -127,4 +127,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.pardir.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
