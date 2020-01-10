52876b0e41b618e9791d8d607df9bd3ec294d01c
Regina Galambos <gina.galambos@gmail.com>
2020-01-10 04:10:30 +0100

    update static path

--- bptracker/bptracker/settings.py
+++ bptracker/bptracker/settings.py
@@ -127,5 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.pardir.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/frontend/static/frontend'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/frontend/static/frontend/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/static/frontend'),
+]
