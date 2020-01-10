0749a1e82baad4c7fcca011875c4781c4a6fd42c
Regina Galambos <gina.galambos@gmail.com>
2020-01-10 03:57:23 +0100

    update static_url

--- bptracker/bptracker/settings.py
+++ bptracker/bptracker/settings.py
@@ -128,4 +128,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.pardir.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/frontend/static/frontend'
